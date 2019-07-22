8ade98f33e253fd32f6bd4e8cb17f76136347589
lapostoj <jerome.lapostolet@gmail.com>
2019-07-22 22:56:49 +0100

    Add wine services

---
+++ service/application/service/createwine/createwine.go
@@ -0,0 +1,22 @@
+package createwine
+
+import (
+	"context"
+
+	"github.com/lapostoj/winemanager/service/domain/model/wine"
+)
+
+// CreateWineService defines the interface for a CreateWineService
+type CreateWineService interface {
+	Execute(ctx context.Context, wine *wine.Wine) (string, error)
+}
+
+// CreateWine implements a service to get Wines
+type CreateWine struct {
+	WineRepository wine.Repository
+}
+
+// Execute persists the passed wine and return its id.
+func (service CreateWine) Execute(ctx context.Context, wine *wine.Wine) (string, error) {
+	return service.WineRepository.SaveWine(ctx, wine)
+}
---
+++ service/application/service/getwines/getwines_test.go
@@ -0,0 +1,46 @@
+package getwines_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/lapostoj/winemanager/service/application/service/getwines"
+	"github.com/lapostoj/winemanager/service/domain/model/wine"
+	"github.com/lapostoj/winemanager/service/test"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockWineRepository struct {
+	mock.Mock
+}
+
+func (mock *MockWineRepository) SaveWine(ctx context.Context, wine *wine.Wine) (string, error) {
+	args := mock.Called(ctx, wine)
+	return args.String(0), args.Error(1)
+}
+
+func (mock *MockWineRepository) SaveTestWine(ctx context.Context) error {
+	args := mock.Called(ctx)
+	return args.Error(0)
+}
+
+func (mock *MockWineRepository) GetWines(ctx context.Context, wines *[]wine.Wine) error {
+	args := mock.Called(ctx, wines)
+	*wines = append(*wines, test.AWine(), test.AWine())
+	return args.Error(0)
+}
+
+func TestGetWines(t *testing.T) {
+	ctx := context.Background()
+	wineRepository := new(MockWineRepository)
+	var wines []wine.Wine
+	getWinesService := getwines.GetWines{WineRepository: wineRepository}
+	wineRepository.On("GetWines", ctx, &wines).Return(nil)
+
+	wines, err := getWinesService.Execute(ctx)
+
+	assert.Nil(t, err)
+	assert.Equal(t, len(wines), 2)
+	wineRepository.AssertCalled(t, "GetWines", ctx, &wines)
+}
